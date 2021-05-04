9830afe282ac0b2dd46ba7e31ac6ed2320664b8b
BrendanFinnegan <bf289907@gmail.com>
2021-05-04 10:48:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+}
+
+const addAnotherEventListener = () => {
+    const x = document.getElementById('main');
+    x.addEventListener('click',() => {alert('Cliky click click');})
 }
